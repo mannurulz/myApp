56feb71abc8033afd73685faf77ea8eb22e7d251
mannurulz <mannurulz@gmail.com>
2017-07-05 01:22:16 +0800

    Commit 2

---
+++ myApp/sample_panda.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
+        'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'],
+        'age': [42, 52, 36, 24, 73],
+        'preTestScore': [4, 24, 31, 2, 3],
+        'postTestScore': [25, 94, 57, 62, 70]}
+df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
+print(df)
+df.take(np.random.permutation(len(df))[:2])
